7fa243f0b19996051686171ce77da11e1d34318d
tejas100 <tejasastendulker@gmail.com>
2019-03-30 21:22:48 +0530

    init

---
+++ hello AR/scripts/ARController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class ARController : MonoBehaviour
+{
+
+    private List<TrackedPlane> m_NewTrackedPlanes = new List<TrackedPlane>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check ARCore session status
+
+        if(Session.Status != SessionStatus.Tracking){
+
+            return;
+        }
+
+        Session.GetTrackables<TrackedPlane>(m_NewTrackedPlanes, TrackableQueryFilter.New);
+
+
+    }
+}
